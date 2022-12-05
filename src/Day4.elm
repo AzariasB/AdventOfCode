14c585d4eebbc47ae30efe036d83e7caab03f010
AzariasB <azarias.boutin@gmail.com>
2022-12-05 17:01:52 +0100

    fyra

--- src/Day4.elm
+++ src/Day4.elm
@@ -1,6 +1,15 @@
 module Day4 exposing (part1, part2)
 
 import Helpers
+import Parser exposing ((|.), (|=), Parser, int, succeed, symbol)
+
+
+
+-- STRUCTS
+
+
+type alias Section =
+    { aStart : Int, aEnd : Int, bStart : Int, bEnd : Int }
 
 
 
@@ -8,13 +17,60 @@ import Helpers
 
 
 part1 : List String -> String
-part1 _ =
-    ""
+part1 sections =
+    sections
+        |> List.map (toSection >> containsItself >> boolToInt)
+        |> List.sum
+        |> String.fromInt
 
 
 part2 : List String -> String
-part2 _ =
-    ""
+part2 sections =
+    sections
+        |> List.map (toSection >> overlaps >> boolToInt)
+        |> List.sum
+        |> String.fromInt
+
+
+rangeParser : Parser Section
+rangeParser =
+    succeed Section
+        |= int
+        |. symbol "-"
+        |= int
+        |. symbol ","
+        |= int
+        |. symbol "-"
+        |= int
+
+
+boolToInt : Bool -> Int
+boolToInt b =
+    if b then
+        1
+
+    else
+        0
+
+
+toSection : String -> Section
+toSection str =
+    case Parser.run rangeParser str of
+        Ok section ->
+            section
+
+        Err _ ->
+            Debug.todo <| "Failed to parse section " ++ str
+
+
+containsItself : Section -> Bool
+containsItself { aStart, aEnd, bStart, bEnd } =
+    aStart >= bStart && aEnd <= bEnd || bStart >= aStart && bEnd <= aEnd
+
+
+overlaps : Section -> Bool
+overlaps { aStart, aEnd, bStart, bEnd } =
+    aStart <= bStart && aEnd >= bStart || bStart <= aStart && bEnd >= aStart
 
 
 
