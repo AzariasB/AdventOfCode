c02771ff16a97159d9d1c8203fdf3793a639e90f
AzariasB <azarias.boutin@gmail.com>
2022-12-23 19:13:50 +0100

    Starting day 23

--- src/Day21.elm
+++ src/Day21.elm
@@ -1,5 +1,6 @@
 module Day21 exposing (part1, part2)
 
+import Arithmetic exposing (gcd)
 import Dict exposing (Dict)
 import Helpers
 import Parser exposing ((|.), (|=), Parser, end, int, oneOf, spaces, succeed, symbol)
@@ -30,6 +31,15 @@ type alias Monkey =
     { name : String, yell : Yell }
 
 
+type alias Ratio =
+    ( Int, Int )
+
+
+type EquationSide
+    = Constant Ratio
+    | LinearEquation Ratio Ratio
+
+
 
 -- SOLUTION
 
@@ -38,15 +48,141 @@ part1 : List String -> String
 part1 monkeys =
     monkeys
         |> runOnList monkeyParser
-        -- |> Debug.log "operations"
         |> monkeysToDict
         |> interpret "root"
         |> String.fromInt
 
 
+
+-- 3453748220115 too low
+
+
 part2 : List String -> String
-part2 _ =
-    "TODO"
+part2 monkeys =
+    monkeys
+        |> runOnList monkeyParser
+        |> monkeysToDict
+        |> resolveEquation "root"
+        |> String.fromInt
+
+
+resolveEquation : String -> Dict String Monkey -> Int
+resolveEquation name jungle =
+    let
+        sides =
+            Dict.get name jungle
+    in
+    case sides of
+        Nothing ->
+            Debug.log (name ++ " not found") 0
+
+        Just { yell } ->
+            case yell of
+                Number i ->
+                    i
+
+                Result { left, right } ->
+                    let
+                        leftEq =
+                            createEquation left jungle |> Debug.log "left"
+
+                        rightEq =
+                            createEquation right jungle |> Debug.log "right"
+                    in
+                    case ( leftEq, rightEq ) of
+                        ( Constant c, LinearEquation a b ) ->
+                            solveEquation c a b
+
+                        ( LinearEquation a b, Constant c ) ->
+                            solveEquation c a b
+
+                        _ ->
+                            Debug.log "unsupported equations" -1
+
+
+
+-- (constantV/constantD) = (aValue/aDivisor)x + (bValue/bDivisor)
+-- x = ((constantV/constantD) - (bValue/bDivisor)) / (aValue/aDivisor)
+
+
+solveEquation : Ratio -> Ratio -> Ratio -> Int
+solveEquation constant a b =
+    let
+        diff =
+            applyRatioOperand Subtract constant b
+
+        ( fact, div ) =
+            applyRatioOperand Divide diff a
+    in
+    mIDiv fact div + 1
+
+
+createEquation : String -> Dict String Monkey -> EquationSide
+createEquation name jungle =
+    if name == "humn" then
+        LinearEquation ( 1, 1 ) ( 0, 1 )
+
+    else
+        let
+            found =
+                Dict.get name jungle
+        in
+        case found of
+            Nothing ->
+                Debug.log (name ++ " not found") Constant ( 1, 1 )
+
+            Just { yell } ->
+                case yell of
+                    Number i ->
+                        Constant ( i, 1 )
+
+                    Result { left, operand, right } ->
+                        let
+                            leftValue =
+                                createEquation left jungle
+
+                            rightValue =
+                                createEquation right jungle
+                        in
+                        mergeEquations leftValue rightValue operand
+
+
+mergeEquations : EquationSide -> EquationSide -> Operand -> EquationSide
+mergeEquations left right op =
+    case ( left, right ) of
+        ( Constant a, Constant b ) ->
+            Constant <| applyRatioOperand op a b
+
+        ( LinearEquation a b, Constant c ) ->
+            case op of
+                Add ->
+                    LinearEquation a (applyRatioOperand Add b c)
+
+                Subtract ->
+                    LinearEquation a (applyRatioOperand Subtract b c)
+
+                Multiply ->
+                    LinearEquation (applyRatioOperand Multiply a c) (applyRatioOperand Multiply b c)
+
+                Divide ->
+                    LinearEquation (applyRatioOperand Divide a c) (applyRatioOperand Divide b c)
+
+        ( Constant c, LinearEquation a b ) ->
+            case op of
+                Add ->
+                    LinearEquation a (applyRatioOperand Add b c)
+
+                Subtract ->
+                    LinearEquation (neg a) (applyRatioOperand Subtract c b)
+
+                Multiply ->
+                    LinearEquation (applyRatioOperand Multiply a c) (applyRatioOperand Multiply b c)
+
+                Divide ->
+                    LinearEquation (applyRatioOperand Divide c a) (applyRatioOperand Divide c b)
+
+        _ ->
+            Debug.log "Unsupported equation" Constant ( 0, 1 )
 
 
 interpret : String -> Dict String Monkey -> Int
@@ -62,7 +198,7 @@ interpret name jungle =
         Just { yell } ->
             case yell of
                 Number i ->
-                    i |> Debug.log ("eval " ++ name ++ " is ")
+                    i
 
                 Result { left, operand, right } ->
                     let
@@ -71,11 +207,37 @@ interpret name jungle =
 
                         rightValue =
                             interpret right jungle
-
-                        res =
-                            applyOperand operand leftValue rightValue |> Debug.log ("eval " ++ name ++ " is ")
                     in
-                    res
+                    applyOperand operand leftValue rightValue
+
+
+applyRatioOperand : Operand -> Ratio -> Ratio -> Ratio
+applyRatioOperand op ( a, b ) ( c, d ) =
+    case op of
+        -- a/b + c/d = (a*d + c*b) / b*d
+        Add ->
+            ( a * d + c * b, b * d ) |> simplify
+
+        -- a/b - c/d = (a*d - c*b) / b*d
+        Subtract ->
+            ( a * d - c * b, b * d ) |> simplify
+
+        -- a/b * c/d = a*c / b*d
+        Multiply ->
+            ( a * c, d * b ) |> simplify
+
+        -- a/b / c/d = a*d / b*c
+        Divide ->
+            ( a * d, b * c ) |> simplify
+
+
+simplify : Ratio -> Ratio
+simplify ( a, b ) =
+    let
+        g =
+            gcd a b
+    in
+    ( mIDiv a g, mIDiv b g )
 
 
 applyOperand : Operand -> Int -> Int -> Int
@@ -91,7 +253,17 @@ applyOperand op x y =
             x * y
 
         Divide ->
-            floor (toFloat x / toFloat y)
+            mIDiv x y
+
+
+mIDiv : Int -> Int -> Int
+mIDiv x y =
+    floor (toFloat x / toFloat y)
+
+
+neg : Ratio -> Ratio
+neg ( a, b ) =
+    ( -a, b )
 
 
 
@@ -156,10 +328,3 @@ operandParser =
 
 main =
     Helpers.solve part1 part2
-
-
-
--- 3453748220116 : javascript
--- 5697701040178 : elm
--- 5697701040178
--- 21120928600114 : python
