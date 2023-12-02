5baa5f0dc2d9db931101df95be834aef95d690b0
azarias.boutin <azarias.boutin@luxquanta.com>
2023-12-02 11:48:41 +0100

    Day 2 solved
    Part 1: 88.0µs
    Part 2: 79.8µs

--- 2023/src/bin/01.rs
+++ 2023/src/bin/01.rs
@@ -30,8 +30,8 @@ fn normalize_digits(input: &str) -> String {
         ("eight", "e8t"),
         ("nine", "n9e"),
     ]
-    .iter()
-    .fold(input.to_string(), |acc, (b, a)| acc.replace(b, a))
+        .iter()
+        .fold(input.to_string(), |acc, (b, a)| acc.replace(b, a))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -48,13 +48,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(54644));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(53348));
     }
 }
---
+++ 2023/src/bin/02.rs
@@ -0,0 +1,102 @@
+use std::cmp::max;
+use advent_of_code::parsers::lines;
+advent_of_code::solution!(2);
+
+const MAX_REDS: u32 = 12;
+const MAX_GREENS: u32 = 13;
+const MAX_BLUES: u32 = 14;
+
+#[derive(Default, Debug)]
+struct GameSet {
+    reds: u32,
+    greens: u32,
+    blue: u32,
+}
+
+impl GameSet {
+    pub fn is_valid(&self) -> bool {
+        self.reds <= MAX_REDS && self.greens <= MAX_GREENS && self.blue <= MAX_BLUES
+    }
+
+    pub fn maximize(&self, other: &GameSet) -> GameSet {
+        GameSet {
+            blue: max(self.blue, other.blue),
+            reds: max(self.reds, other.reds),
+            greens: max(self.greens, other.greens),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    sets: Vec<GameSet>,
+}
+
+impl Game {
+    pub fn is_valid(&self) -> bool {
+        self.sets.iter().all(GameSet::is_valid)
+    }
+
+    pub fn power(&self) -> u32 {
+        let max_set = self.sets.iter().fold(GameSet::default(), |prev, curr| { prev.maximize(curr) });
+        max_set.reds * max_set.greens * max_set.blue
+    }
+}
+
+fn parse_game_set(input: &str) -> Vec<GameSet> {
+    input.split(";").map(|s| {
+        let mut set = GameSet::default();
+        s.split(", ").for_each(|slice| {
+            let (num, color) = slice.trim().split_once(' ').expect(format!("Failed to parse game set {}", slice).as_str());
+            let count = num.parse::<u32>().expect("Unable to parse cube count");
+            match color {
+                "red" => { set.reds += count }
+                "blue" => { set.blue += count }
+                "green" => { set.greens += count }
+                _ => panic!("Unknown color {}", color)
+            }
+        });
+        set
+    }).collect()
+}
+
+fn parse_game(input: &str) -> Game {
+    let mut splited = input.split(": ");
+    let game_id = splited.next().expect("Game id not found");
+    let game_sets = splited.next().expect("Game data not found");
+    Game {
+        id: game_id[5..].parse::<u32>().expect(format!("Invalid game id {}", game_id).as_str()),
+        sets: parse_game_set(game_sets),
+    }
+}
+
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        lines(input).map(parse_game).filter(Game::is_valid).map(|g| g.id).sum()
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        lines(input).map(parse_game).map(|v| v.power()).sum()
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(2512));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(67335));
+    }
+}
